1a51226494dcb5466c7d135240b7f20ea20849dc
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2023-02-24 23:28:03 +0700

    add sleep tracker serializer

---
+++ app/serializers/sleep_tracker_serializer.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: sleep_trackers
+#
+#  id         :integer          not null, primary key
+#  clock_in   :datetime         not null
+#  sleep_type :string           default("sleep")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_sleep_trackers_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
+class SleepTrackerSerializer < ApplicationSerializer
+  attributes :user, :sleep_type, :clock_in, :created_at, :updated_at
+end
