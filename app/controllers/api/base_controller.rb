297852d84d03adf6b8ad001b710eb31d281f5533
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2023-02-25 19:10:47 +0700

    fixed rubocop (#7)

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -6,6 +6,7 @@ module Api
 
     def authenticate_user!
       return responder(:unauthorized, 'Please provide user_id in headers') unless request.headers['User-Id']
+
       @current_user ||= User.find(request.headers['User-Id'])
     end
   end
