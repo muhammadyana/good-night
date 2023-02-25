4d4ad456529489f1384881da5c62c521089bbb46
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2023-02-25 19:08:22 +0700

    add unfollow method (#7)

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,15 @@
 module Api
   class UsersController < BaseController
-    before_action :set_user
+    before_action :set_user, only: %i[follow unfollow]
 
     def follow
       @current_user.follow(@user)
       responder(:ok, "You're now following #{@user.name}")
+    end
+
+    def unfollow
+      @current_user.unfollow(@user)
+      responder(:ok, "You're now unfollow #{@user.name}")
     end
 
     private
